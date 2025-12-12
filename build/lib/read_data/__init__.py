f07fd479e6ece79621c362697785e228126012a4
Suo-Lab <suo_shengbao@gzlab.ac.cn>
2025-12-12 11:30:22 +0800

    Delete build/lib/read_data directory

--- build/lib/read_data/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ['read']
-
-import diopy
-
-from .read import diopy_read
--- build/lib/read_data/read.py
+++
@@ -1,4 +0,0 @@
-def diopy_read(filename, assay_name = 'RNA'):
-    """Read diopy converted Seurat data"""
-    adata = diopy.input.read_h5(file = filename, assay_name = assay_name)
-    return adata
